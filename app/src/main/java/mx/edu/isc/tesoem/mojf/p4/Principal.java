70394e73a326320ef3057502d8b6086b0b655e01
Francisco <mendxza12@gmail.com>
2023-09-23 20:41:13 -0600

    Update 1

--- app/src/main/java/mx/edu/isc/tesoem/mojf/p4/Principal.java
+++ app/src/main/java/mx/edu/isc/tesoem/mojf/p4/Principal.java
@@ -1,33 +1,42 @@
 package mx.edu.isc.tesoem.mojf.p4;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import DatosParce.DatosParce;
+
 public class Principal extends AppCompatActivity{
 
-    Button btnab;
-    EditText txtnombre;
+    Button btnab, btnap;
+    EditText txtnombre, txtedad, txtcorreo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
         txtnombre = findViewById(R.id.p1txtnombre);
+        txtcorreo = findViewById(R.id.p1txtcorreo);
+        txtedad = findViewById(R.id.p1txtedad);
 
         btnab = findViewById(R.id.p1btnab);
-        btnab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent lanza = new Intent(Principal.this, Basica.class);
-                lanza.putExtra( "nombre", txtnombre.getText().toString());
-                startActivity(lanza);
-            }
+        btnap = findViewById(R.id.p1btnap);
+        btnab.setOnClickListener(view -> {
+            Intent lanza = new Intent(Principal.this, Basica.class);
+            lanza.putExtra( "nombre", txtnombre.getText().toString());
+            startActivity(lanza);
         });
+
+       btnap.setOnClickListener(view -> {
+           DatosParce datosParce = new DatosParce(txtnombre.getText().toString(), txtcorreo.getText().toString(), Integer.parseInt(txtedad.getText().toString()));
+           Intent lanza = new Intent(Principal.this, Parce.class);
+           lanza.putExtra("datosparcelables", datosParce);
+           startActivity(lanza);
+       });
     }
 
 
