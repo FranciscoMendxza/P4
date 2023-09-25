7b5c696cd6692bed6fc6b72e2e9da566204088a8
Francisco <mendxza12@gmail.com>
2023-09-25 17:11:28 -0600

    Update 2

--- app/src/main/java/mx/edu/isc/tesoem/mojf/p4/Principal.java
+++ app/src/main/java/mx/edu/isc/tesoem/mojf/p4/Principal.java
@@ -2,17 +2,32 @@ package mx.edu.isc.tesoem.mojf.p4;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import DatosParce.DatosParce;
 
 public class Principal extends AppCompatActivity{
 
-    Button btnab, btnap;
+    Button btnab, btnap, btnar;
     EditText txtnombre, txtedad, txtcorreo;
+
+
+    ActivityResultLauncher<Intent> activityResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+        if (result.getResultCode() == RESULT_OK){
+            Toast.makeText(Principal.this, "Regresaste con exito, amor", Toast.LENGTH_SHORT).show();
+        }else if(result.getResultCode() == RESULT_CANCELED){
+            Toast.makeText(Principal.this, "Cancelao por el usuario pa", Toast.LENGTH_SHORT).show();
+        }
+    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +40,8 @@ public class Principal extends AppCompatActivity{
 
         btnab = findViewById(R.id.p1btnab);
         btnap = findViewById(R.id.p1btnap);
+        btnar = findViewById(R.id.p1btnar);
+
         btnab.setOnClickListener(view -> {
             Intent lanza = new Intent(Principal.this, Basica.class);
             lanza.putExtra( "nombre", txtnombre.getText().toString());
@@ -36,6 +53,11 @@ public class Principal extends AppCompatActivity{
            Intent lanza = new Intent(Principal.this, Parce.class);
            lanza.putExtra("datosparcelables", datosParce);
            startActivity(lanza);
+       });
+
+       btnar.setOnClickListener(view -> {
+           Intent lanza = new Intent(Principal.this, Regresa.class);
+           activityResult.launch(lanza);
        });
     }
 
